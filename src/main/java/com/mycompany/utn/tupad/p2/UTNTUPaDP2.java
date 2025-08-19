0cd3016e7aa580f8c8dd70c6a36931e280c6e90a
donramones <agusrivarolaa00@gmail.com>
2025-08-19 20:24:05 -0300

    carga inicial

---
+++ src/main/java/com/mycompany/utn/tupad/p2/UTNTUPaDP2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.utn.tupad.p2;
+
+/**
+ *
+ * @author rivar
+ */
+public class UTNTUPaDP2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
