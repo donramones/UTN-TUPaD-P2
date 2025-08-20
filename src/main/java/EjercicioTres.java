c8c37395ab96656ffcb04bb58363e16813df4779
donramones <agusrivarolaa00@gmail.com>
2025-08-19 22:07:54 -0300

    ejercicioTres

--- src/main/java/EjercicioTres.java
+++ src/main/java/EjercicioTres.java
@@ -9,4 +9,19 @@
  */
 public class EjercicioTres {
     
+    public static void main(String[] args) {
+        // a. Declaración e inicialización de la variable String
+        String nombre = 'Ana';
+        // b. Declaración e inicialización de la variable int
+        int edad = 25;
+        // c. Declaración e inicialización de la variable double
+        double altura = 1.69;
+        // d. Declaración e inicialización de la variable boolean
+        boolean estudiante = true;
+        // Impresión de los valores en la pantalla
+        System.out.println("Nombre" + nombre);
+        System.out.println("Edad" + edad);
+        System.out.println("Altura" + altura + " metros");
+        System.out.println("Es estudiante: " + estudiante);
+    }
 }
