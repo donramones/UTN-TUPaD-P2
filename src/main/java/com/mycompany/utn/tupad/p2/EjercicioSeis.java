2fb4a56323a377e8725d28573f3e82b7f4ae25fe
donramones <agusrivarolaa00@gmail.com>
2025-09-11 23:55:31 -0300

    6ok

--- src/main/java/com/mycompany/utn/tupad/p2/EjercicioSeis.java
+++ src/main/java/com/mycompany/utn/tupad/p2/EjercicioSeis.java
@@ -9,5 +9,10 @@ package com.mycompany.utn.tupad.p2;
  * @author rivar
  */
 public class EjercicioSeis {
+    public static void main(String args[]) {
+        System.out.println("Nombre: Juan Perez\n");
+        System.out.println("Edad: 30 años\n");
+        System.out.println("Dirección: \"Calle Falsa 123\"\n");
+    }
     
 }
