d05001d4200f0d0968c8c2c20ed572085da7d644
donramones <agusrivarolaa00@gmail.com>
2025-09-12 00:20:48 -0300

    10ok

--- src/main/java/com/mycompany/utn/tupad/p2/EjercicioNueve.java
+++ src/main/java/com/mycompany/utn/tupad/p2/EjercicioNueve.java
@@ -8,6 +8,13 @@ package com.mycompany.utn.tupad.p2;
  *
  * @author rivar
  */
+import java.util.Scanner;
 public class EjercicioNueve {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Ingresa tu nombre: ");
+        String nombre = scanner.nextLine(); //scanner.nextInt() se utiliza para numeros enteros en cambio scanner.nextLine() lee toda la línea de texto que el usuario ingresa.
+        System.out.println("Hola, " + nombre);
+    }
     
 }
