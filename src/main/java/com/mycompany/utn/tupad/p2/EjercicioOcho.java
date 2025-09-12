5d96ac3102f12156156a18a61684c07c0bdc9236
donramones <agusrivarolaa00@gmail.com>
2025-09-12 00:10:20 -0300

    8ok

--- src/main/java/com/mycompany/utn/tupad/p2/EjercicioOcho.java
+++ src/main/java/com/mycompany/utn/tupad/p2/EjercicioOcho.java
@@ -8,6 +8,20 @@ package com.mycompany.utn.tupad.p2;
  *
  * @author rivar
  */
+import java.util.Scanner;
 public class EjercicioOcho {
+    public static void main(String args[]) {
+    Scanner input = new Scanner(System.in);
+    System.out.println("Dividir de números enteros");
+    System.out.println("Ingresa el primer número (entero): ");
+    Double num1 = Double.parseDouble(input.nextLine());
+    System.out.print("Ingresa el segundo número (entero): ");
+    Double num2 = Double.parseDouble(input.nextLine());
+    
+    Double resultadoEntero = num1 / num2;
+    System.out.println("El resultado de la división entera es: " + resultadoEntero);
+    
+         
+    }
     
 }
