b26b57aec6e3ff71e222dccd71fc8781a3a616c7
donramones <agusrivarolaa00@gmail.com>
2025-09-12 00:20:37 -0300

    10ok

--- src/main/java/com/mycompany/utn/tupad/p2/EjercicioDiez.java
+++ src/main/java/com/mycompany/utn/tupad/p2/EjercicioDiez.java
@@ -9,5 +9,11 @@ package com.mycompany.utn.tupad.p2;
  * @author rivar
  */
 public class EjercicioDiez {
+    public static void main(String args[]) {
+        int a = 5;
+        int b = 2;
+        int resultado = a / b;
+        System.out.println("Resultado: " + resultado);
+    }
     
 }
